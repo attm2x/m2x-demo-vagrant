79a228ff5c315d38019a5109eb31ad2ae0bc6d1c
Federico Saravia <fedesaravia@gmail.com>
2015-10-23 20:27:10 -0300

    Fix stockreport.py script

--- stockreport.py
+++ stockreport.py
@@ -12,40 +12,40 @@ from m2x.client import M2XClient
 TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 print("%s: Starting stockreport.py run" % time.strftime(TIMEFORMAT))
 
-DEVICE_NAME = "stockreport-heroku"
+DEVICE_NAME = "stockreport-vagrant-demo"
 
 # Load config
-APIKEY = open(os.environ['OPENSHIFT_REPO_DIR'] + 'm2x_api_key.txt').read().strip()
+APIKEY = open('/vagrant/m2x_api_key.txt').read().strip()
 now = datetime.datetime.now()
 ATT_Stock_Price = ystockquote.get_price('T')
 
 # Now let's create a device:
 client = M2XClient(key=APIKEY)
 stockreport_device_exists = False
-for device in client.devices:
+for device in client.devices():
     if device.name == DEVICE_NAME:
         stockreport_device_exists = True
         break
 
 if not stockreport_device_exists:
-    device = client.devices.create(
+    device = client.create_device(
         name=DEVICE_NAME,
         description="Stockreport Example Device",
         visibility="private")
 
 # Now we need to get the stream for AT&T's stock ticker symbol ("T")
 ATT_Stream_Exists = False
-for stream in device.streams:
+for stream in device.streams():
     if stream.name.upper() == "T":
         ATT_Stream_Exists = True
         break
 
 if not ATT_Stream_Exists:
-    stream = device.streams.create('T')
+    stream = device.create_stream('T')
 
 stream.update(unit={'label': 'Dollars', 'symbol': '$'})
 
-stream.values.add_value(ATT_STOCK_PRICE, now)
+stream.add_value(ATT_Stock_Price, now)
 
 print("Ending stockreport.py run")
 print
